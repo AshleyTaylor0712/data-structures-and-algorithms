78cc9ac6bc6a2fcc9246b3ebc575a5a67d694498
Ashley Taylor <blueberrypea@gmail.com>
2023-06-16 19:43:22 -0400

    lab 5

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,15 +1,38 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
 class LinkedList:
     """
-    Put docstring here
+
     """
-
     def __init__(self):
-        # initialization here
-        pass
+     self.head = None
+     self.tail = None
+     self.size = 0
 
-    def some_method(self):
-        # method body here
-        pass
+    def is_empty(self):
+      return self.head is None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+           self.tail.next = new_node
+           self.tail = new_node
+
+        self.size += 1
+        
+def test_to_string_empty():
+  linked_list = LinkedList()
+
+  assert str(linked_list) == "NULL"
+
+
+pass
 
 
 class TargetError:
