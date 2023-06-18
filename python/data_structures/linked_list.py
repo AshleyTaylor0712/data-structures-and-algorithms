bd15bd65fa9330d6fefeeb89febf544011efaf32
Ashley Taylor <blueberrypea@gmail.com>
2023-06-18 10:36:52 -0400

    edits

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -12,9 +12,12 @@ class LinkedList:
      self.tail = None
      self.size = 0
 
+    # this checks if the list in empty
     def is_empty(self):
-      return self.head is None
+      if self.head is None:
+          return None
 
+    #inserts a new value at the tail of linkedlist
     def insert(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -25,11 +28,18 @@ class LinkedList:
            self.tail = new_node
 
         self.size += 1
-        
-def test_to_string_empty():
-  linked_list = LinkedList()
 
-  assert str(linked_list) == "NULL"
+    def __str__(self):
+        if self.head is None:
+            return "NULL"
+        else:
+            node = self.head
+            values = []
+            while node is not None:
+                values.append("{ " + str(node.value) + " }")
+                node = node.next
+            values.append("NULL")
+            return " -> ".join(values)
 
 
 pass
@@ -37,3 +47,9 @@ pass
 
 class TargetError:
     pass
+
+if __name__ == "__main__":
+    linked = LinkedList()
+    #linked.insert("apple")
+    #print (linked.head.value)
+    print (linked.__str__())
